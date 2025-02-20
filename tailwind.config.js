9f5de4aa4a6037e6460e229a8ac7a085d1fc5edb
shreevatsatg <tgshreevatsa@gmail.com>
2025-02-20 11:36:41 +0530

    added darkmode option

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,3 +11,11 @@ export default {
 }
 
 
+module.exports = {
+  darkMode: "class", // Enable dark mode using the "class" strategy
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
