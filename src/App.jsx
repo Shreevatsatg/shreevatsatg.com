68b66c27da212c7bb4b2195d3e6d1487e8c5a7c1
shreevatsatg <tgshreevatsa@gmail.com>
2025-02-28 10:35:57 +0530

    added vercel analitics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/react';
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
 import Blog from "./pages/blog";
@@ -35,6 +36,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/drawings" element={<Drawings />} />
       </Routes>
+      <Analytics />
     </div>
   );
 }
